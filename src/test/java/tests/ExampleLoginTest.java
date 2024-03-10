061b02a6e6a14bd1c75af43ad1adcd3266d5c34f
Kwlad1ck <vlad-kad00@mail.ru>
2024-03-10 18:34:52 +0300

    add example login test

---
+++ src/test/java/tests/ExampleLoginTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.is;
+
+public class ExampleLoginTest {
+
+    @Test
+    void negativeLoginNoContentTypeTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .body("{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void successfulLoginWithContentTypeTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .contentType(JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/success-login-schema.json"))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+}
